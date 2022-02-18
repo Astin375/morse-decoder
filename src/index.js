a8a1567bf19f96df0bffff651c9a86ad4a00f873
Artsemi <stojanka25@gmail.com>
2022-02-19 01:50:31 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,23 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// const expression = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
 function decode(expr) {
     // write your solution here
+    let chars = expr.match(/.{1,10}/g);
+    for (let i = 0; i < chars.length; i++){
+        chars[i] = chars[i].replace(/00/g, '');
+        chars[i] = chars[i].replace(/10/g, '.');
+        chars[i] = chars[i].replace(/11/g, '-');
+        chars[i] = chars[i].replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');
+        chars[i] === ' ' ? null : chars[i] = MORSE_TABLE[chars[i]];
+    }
+    // console.log(chars.join(''));
+    return chars.join('');
 }
 
+// decode(expression);
 module.exports = {
     decode
 }
